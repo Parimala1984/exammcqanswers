ec08ae9d8a190c511781dbca72e7ab28825e09d8
Parimala V <parimalav379@gmail.com>
2024-08-08 19:05:32 +0530

    Add files via upload

---
+++ mcq.py
@@ -0,0 +1,69 @@
+#question 1
+#what is the output of this expression,3*1**3?
+print(3*1**3)
+
+
+#question 2
+#which of the following declarations is inncorret in python language?
+#option B is the answer
+x y z p=5,000,000
+print(x y z)
+
+
+#question 3
+#which of the is not a complex number?
+#option C is the answer
+k=2+3i
+print(k)
+
+
+# what is the output of the following peice of code?
+#option b is the answer
+my_dict={1:"A",2:"B",3:"C"}
+print(my_dict.get(1,4))
+
+
+# what error occurs when you execute?
+#option B is the answer
+apple="mango"
+
+
+# what is the output of the following?
+#option C is the answer
+print("xyyzxyzxzxyy".count('yy',5))
+
+
+#what is the expected output?
+# option D is the answer
+import random
+x=random.random()
+y=random.randint(0,4)
+print(int(x),":",y+int(x))
+#output 0:3
+
+
+#question 8
+# which of the following statments are correct?
+# option A is the answer
+a=True
+b=False
+c=False
+if a or b and c:
+    print("HOPE FOUNDATION")
+else:
+    print("hope foundation")
+
+
+#question 9
+#what is the output of the below program in python?
+#option B is the answer
+print(0.2+0.4==0.6)
+#output False
+
+
+#question 10
+#what is the output of below python code?
+my_str=['ram',"has","red",'cap']
+list=''.join(my_str)
+print(my_str)
+#option D is the answer
